37b4451b27cf2491ef38ea23bab82e0f8a4aa9e9
Omar Rizwan <omar.rizwan@gmail.com>
2017-10-06 20:41:42 -0700

    Draw a fill overlay.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react';
+import * as React from "react";
 
-import ReactMapboxGl from 'react-mapbox-gl';
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 
-import './App.css';
+import "./App.css";
 
 const Map = ReactMapboxGl({
   accessToken:
-    'pk.eyJ1Ijoib3NuciIsImEiOiJjajZwanhjYmwwMDB3MnhxcG9ocHJmMDBsIn0.T8Xi_6tEDq_4Yf3YSYLEPA',
+    "pk.eyJ1Ijoib3NuciIsImEiOiJjajZwanhjYmwwMDB3MnhxcG9ocHJmMDBsIn0.T8Xi_6tEDq_4Yf3YSYLEPA"
 });
 
 class App extends React.Component {
@@ -17,10 +17,29 @@ class App extends React.Component {
           style="mapbox://styles/mapbox/outdoors-v9"
           center={[-122.4194, 37.7749]}
           containerStyle={{
-            height: '100vh',
-            width: '100vw',
+            height: "100vh",
+            width: "100vw"
           }}
-        />
+        >
+          <Layer
+            type="fill"
+            id="marker"
+            paint={{
+              "fill-color": "green",
+              "fill-opacity": 0.7
+            }}
+          >
+            <Feature
+              coordinates={[
+                [
+                  [-122.4194, 37.7749],
+                  [-122.5194, 37.7749],
+                  [-122.4194, 37.7849]
+                ]
+              ]}
+            />
+          </Layer>
+        </Map>
       </div>
     );
   }
