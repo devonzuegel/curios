6859619da1677b8bc2f3e145d9a8444327fcd607
Omar Rizwan <omar.rizwan@gmail.com>
2017-10-06 20:17:04 -0700

    Render SF map.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import * as React from 'react';
-import './App.css';
+import * as React from "react";
 
-const logo = require('./logo.svg');
+import ReactMapboxGl from "react-mapbox-gl";
+
+import "./App.css";
+
+const logo = require("./logo.svg");
+
+const Map = ReactMapboxGl({
+  accessToken:
+    "pk.eyJ1Ijoib3NuciIsImEiOiJjajZwanhjYmwwMDB3MnhxcG9ocHJmMDBsIn0.T8Xi_6tEDq_4Yf3YSYLEPA"
+});
 
 class App extends React.Component {
   render() {
@@ -11,9 +19,16 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p className="App-intro">hello</p>
+
+        <Map
+          style="mapbox://styles/mapbox/streets-v9"
+          center={[-122.4194, 37.7749]}
+          containerStyle={{
+            height: "300px",
+            width: "500px"
+          }}
+        />
       </div>
     );
   }
