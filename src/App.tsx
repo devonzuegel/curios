5c69fc1a480bdbd2a841b851baedac63138ba045
Devon Kristine Zuegel <devonzuegel@gmail.com>
2017-10-06 20:27:45 -0700

    feat: Full screen map

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,24 @@
-import * as React from "react";
+import * as React from 'react';
 
-import ReactMapboxGl from "react-mapbox-gl";
+import ReactMapboxGl from 'react-mapbox-gl';
 
-import "./App.css";
-
-const logo = require("./logo.svg");
+import './App.css';
 
 const Map = ReactMapboxGl({
   accessToken:
-    "pk.eyJ1Ijoib3NuciIsImEiOiJjajZwanhjYmwwMDB3MnhxcG9ocHJmMDBsIn0.T8Xi_6tEDq_4Yf3YSYLEPA"
+    'pk.eyJ1Ijoib3NuciIsImEiOiJjajZwanhjYmwwMDB3MnhxcG9ocHJmMDBsIn0.T8Xi_6tEDq_4Yf3YSYLEPA',
 });
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">hello</p>
-
         <Map
           style="mapbox://styles/mapbox/streets-v9"
           center={[-122.4194, 37.7749]}
           containerStyle={{
-            height: "300px",
-            width: "500px"
+            height: '100vh',
+            width: '100vw',
           }}
         />
       </div>
