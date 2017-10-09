d1c1a0c736b66699aba11993a3ee06d6a7b4540a
Devon Kristine Zuegel <devonzuegel@gmail.com>
2017-10-08 23:32:27 -0700

    feat: Add placeholder Salesforce Tower data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import * as React from "react";
+import * as React from 'react';
 
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import ReactMapboxGl, { Layer, Feature, Marker } from 'react-mapbox-gl';
+import salesforceTower from './data/salesforceTower';
 
-import "./App.css";
+import './App.css';
 
 const Map = ReactMapboxGl({
   accessToken:
-    "pk.eyJ1Ijoib3NuciIsImEiOiJjajZwanhjYmwwMDB3MnhxcG9ocHJmMDBsIn0.T8Xi_6tEDq_4Yf3YSYLEPA"
+    'pk.eyJ1Ijoib3NuciIsImEiOiJjajZwanhjYmwwMDB3MnhxcG9ocHJmMDBsIn0.T8Xi_6tEDq_4Yf3YSYLEPA',
 });
 
 class App extends React.Component {
@@ -17,28 +18,29 @@ class App extends React.Component {
           style="mapbox://styles/mapbox/outdoors-v9"
           center={[-122.4194, 37.7749]}
           containerStyle={{
-            height: "100vh",
-            width: "100vw"
+            height: '100vh',
+            width: '100vw',
           }}
         >
           <Layer
-            type="fill"
-            id="marker"
+            type="circle"
             paint={{
-              "fill-color": "green",
-              "fill-opacity": 0.7
+              'circle-radius': 9,
+              'circle-color': '#E54E52',
+              'circle-opacity': 0.5,
             }}
           >
-            <Feature
-              coordinates={[
-                [
-                  [-122.4194, 37.7749],
-                  [-122.5194, 37.7749],
-                  [-122.4194, 37.7849]
-                ]
-              ]}
-            />
+            <Feature coordinates={salesforceTower.locations[0]} />
           </Layer>
+
+          <Marker
+            style={{ cursor: 'pointer' }}
+            onClick={() => alert(JSON.stringify(salesforceTower, null, 2))}
+            coordinates={salesforceTower.locations[0] as [number, number]}
+            anchor="bottom"
+          >
+            <h1>{salesforceTower.names[0]}</h1>
+          </Marker>
         </Map>
       </div>
     );
---
+++ src/data/salesforceTower.ts
@@ -0,0 +1,36 @@
+import { Money, Currencies } from 'ts-money';
+import { TProject } from './project.d';
+
+const oneBillion = 1000000000.0;
+
+const salesforceTower: TProject = {
+  names: ['Salesforce Tower'],
+  keyDates: [
+    { date: new Date('2018'), title: 'Expected completion date', notes: [] },
+  ],
+  locations: [[-122.3969, 37.7899]],
+  budgets: [
+    Money.fromDecimal(oneBillion, Currencies.USD),
+    Money.fromDecimal(oneBillion * 1.1, Currencies.USD),
+  ],
+  categories: ['building', 'construction', 'private', 'skyscraper'],
+  notes: [
+    '1,070-foot (326 m) tall',
+    'Salesforce Tower is the centerpiece of the San Francisco Transbay redevelopment plan.',
+  ],
+  players: [
+    'Salesforce',
+    'Boston Properties (owner — 95%)',
+    'Hines Interests LP (owner — 5%)',
+    'Pelli Clarke Pelli Architects',
+  ],
+  mediaUrls: [
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/' +
+      'Salesforce_Tower_during_construction%2C_January_2017.jpg/' +
+      '1920px-Salesforce_Tower_during_construction%2C_January_2017.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/a/a3/Salesforce_Tower_April_2017.jpg',
+  ],
+  resourceUrls: ['https://en.wikipedia.org/wiki/Salesforce_Tower'],
+};
+
+export default salesforceTower;
