d3f3a9daa8ddc630103e95112ba9aed316c15a97
Devon Kristine Zuegel <devonzuegel@gmail.com>
2017-10-10 15:58:22 -0700

    fix: Mock react-mapbox-gl in App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,13 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import App from './App'
 
+jest.mock('react-mapbox-gl', () => ({
+  default: () => () => 'Map',
+  Layer: 'Layer',
+  Feature: 'Feature',
+  Marker: 'Marker',
+}))
+
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
