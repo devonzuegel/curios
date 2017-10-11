38417f73b6a5bbb3e74f4d984397f63826a0a55a
Devon Kristine Zuegel <devonzuegel@gmail.com>
2017-10-11 00:43:42 -0700

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,105 @@
 import * as React from 'react'
+import ReactMapboxGl, {GeoJSONLayer} from 'react-mapbox-gl'
+import * as MapboxGL from 'mapbox-gl'
 
-import ReactMapboxGl, {Layer, Feature, Marker} from 'react-mapbox-gl'
-import salesforceTower from './data/salesforceTower'
-
-import './App.css'
+// tslint:disable-next-line:no-var-requires
+const geojson: GeoJSON.FeatureCollection<
+  GeoJSON.GeometryObject
+> = require('./geojson.json')
 
 const Map = ReactMapboxGl({
+  // TODO: extract into config
   accessToken:
     'pk.eyJ1Ijoib3NuciIsImEiOiJjajZwanhjYmwwMDB3MnhxcG9ocHJmMDBsIn0.T8Xi_6tEDq_4Yf3YSYLEPA',
 })
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Map
-          style="mapbox://styles/mapbox/outdoors-v9"
-          center={[-122.4194, 37.7749]}
-          containerStyle={{
-            height: '100vh',
-            width: '100vw',
-          }}
-        >
-          <Layer
-            type="circle"
-            paint={{
-              'circle-radius': 9,
-              'circle-color': '#E54E52',
-              'circle-opacity': 0.5,
-            }}
-          >
-            <Feature coordinates={salesforceTower.locations[0]} />
-          </Layer>
+const mapStyle = {
+  height: '100%',
+  width: '100%',
+}
 
-          <Marker
-            style={{cursor: 'pointer'}}
-            onClick={() => alert(JSON.stringify(salesforceTower, null, 2))}
-            coordinates={salesforceTower.locations[0] as [number, number]}
-            anchor="bottom"
+const symbolLayout: MapboxGL.SymbolLayout = {
+  'text-field': '{place}',
+  'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+  'text-offset': [0, 0.6],
+  'text-anchor': 'top',
+}
+const symbolPaint: MapboxGL.SymbolPaint = {}
+const circleLayout: MapboxGL.CircleLayout = {visibility: 'visible'}
+const circlePaint: MapboxGL.CirclePaint = {}
+
+class GeoJsonLayer extends React.Component<{}, {}> {
+  private center = [-122.4194, 37.7749]
+
+  public render() {
+    return (
+      <div>
+        <div className="sidebar">
+          {geojson.features.map((store, k) => (
+            <div className="listing" key={k}>
+              {store.properties.place}
+            </div>
+          ))}
+        </div>
+        <div id="map" className="map pad2">
+          <Map
+            style="mapbox://styles/mapbox/outdoors-v9"
+            center={this.center}
+            containerStyle={mapStyle}
           >
-            <h1>{salesforceTower.names[0]}</h1>
-          </Marker>
-        </Map>
+            <GeoJSONLayer
+              data={geojson}
+              circleLayout={circleLayout}
+              circlePaint={circlePaint}
+              circleOnMouseEnter={() => console.log('laksdfjalskdjfasdf')}
+              circleOnClick={this.onClickCircle}
+              symbolLayout={symbolLayout}
+              symbolPaint={symbolPaint}
+            />
+          </Map>
+        </div>
       </div>
     )
   }
+
+  private onClickCircle = (
+    evt: GeoJSON.FeatureCollection<GeoJSON.DirectGeometryObject>
+  ) => {
+    console.log(evt.features[0].geometry.coordinates)
+  }
 }
 
-export default App
+export default GeoJsonLayer
+// import * as React from 'react'
+
+// import ReactMapboxGl, {GeoJSONLayer} from 'react-mapbox-gl'
+// const stores = require('./geojson.json')
+
+// // import salesforceTower from './data/salesforceTower'
+
+// import './App.css'
+
+// const Map = ReactMapboxGl({
+//   accessToken:
+//     'pk.eyJ1Ijoib3NuciIsImEiOiJjajZwanhjYmwwMDB3MnhxcG9ocHJmMDBsIn0.T8Xi_6tEDq_4Yf3YSYLEPA',
+// })
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <div className="sidebar pad2">Listing</div>
+//         <div id="map" className="map pad2">
+//           <Map
+//             style="mapbox://styles/mapbox/outdoors-v9"
+//             center={[-77.034084, 38.909671]}
+//           >
+//             <GeoJSONLayer data={stores} />
+//           </Map>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
