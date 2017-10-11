76dea01014a4e6eaf817166f0efa157e9dc5bb74
Devon Kristine Zuegel <devonzuegel@gmail.com>
2017-10-11 00:56:51 -0700

    working! need to update data

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import * as MapboxGL from 'mapbox-gl'
 
 // tslint:disable-next-line:no-var-requires
 const geojson: GeoJSON.FeatureCollection<
-  GeoJSON.GeometryObject
+  GeoJSON.DirectGeometryObject
 > = require('./geojson.json')
 
 const Map = ReactMapboxGl({
@@ -29,29 +29,29 @@ const circleLayout: MapboxGL.CircleLayout = {visibility: 'visible'}
 const circlePaint: MapboxGL.CirclePaint = {}
 
 class GeoJsonLayer extends React.Component<{}, {}> {
-  private center = [-122.4194, 37.7749]
+  state = {center: [-122.4194, 37.7749], zoom: [12]}
 
   public render() {
     return (
       <div>
         <div className="sidebar">
           {geojson.features.map((store, k) => (
-            <div className="listing" key={k}>
+            <div className="listing" onClick={() => this.flyToStore(store)} key={k}>
               {store.properties.place}
             </div>
           ))}
         </div>
         <div id="map" className="map pad2">
           <Map
+            zoom={this.state.zoom}
             style="mapbox://styles/mapbox/outdoors-v9"
-            center={this.center}
+            center={this.state.center}
             containerStyle={mapStyle}
           >
             <GeoJSONLayer
               data={geojson}
               circleLayout={circleLayout}
               circlePaint={circlePaint}
-              circleOnMouseEnter={() => console.log('laksdfjalskdjfasdf')}
               circleOnClick={this.onClickCircle}
               symbolLayout={symbolLayout}
               symbolPaint={symbolPaint}
@@ -62,44 +62,16 @@ class GeoJsonLayer extends React.Component<{}, {}> {
     )
   }
 
+  private flyToStore(currentFeature: GeoJSON.Feature<GeoJSON.DirectGeometryObject>) {
+    this.setState({center: currentFeature.geometry.coordinates, zoom: [15]})
+  }
+
   private onClickCircle = (
     evt: GeoJSON.FeatureCollection<GeoJSON.DirectGeometryObject>
   ) => {
     console.log(evt.features[0].geometry.coordinates)
+    this.flyToStore(evt.features[0])
   }
 }
 
 export default GeoJsonLayer
-// import * as React from 'react'
-
-// import ReactMapboxGl, {GeoJSONLayer} from 'react-mapbox-gl'
-// const stores = require('./geojson.json')
-
-// // import salesforceTower from './data/salesforceTower'
-
-// import './App.css'
-
-// const Map = ReactMapboxGl({
-//   accessToken:
-//     'pk.eyJ1Ijoib3NuciIsImEiOiJjajZwanhjYmwwMDB3MnhxcG9ocHJmMDBsIn0.T8Xi_6tEDq_4Yf3YSYLEPA',
-// })
-
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="sidebar pad2">Listing</div>
-//         <div id="map" className="map pad2">
-//           <Map
-//             style="mapbox://styles/mapbox/outdoors-v9"
-//             center={[-77.034084, 38.909671]}
-//           >
-//             <GeoJSONLayer data={stores} />
-//           </Map>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-
-// export default App
