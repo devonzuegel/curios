95ee0e2fbb2f03e860c0945292e17796ea09d9a5
Devon Kristine Zuegel <devonzuegel@gmail.com>
2017-10-14 17:21:54 -0700

    refactor: Move <CuriosMap /> + dependencies into separate dir

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import data from './data'
-import CuriosMap from './CuriosMap'
+import CuriosMap from './CuriosMap/'
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 
